eb8b389d827c40c910c25931935dc0c90fd0e12f
MasoumehMehrjoo <MasoumehMehrjoo@outlook.com>
2020-11-07 11:03:11 -0500

    Initial

--- Bahar.DemoApp.InventoryService.Model/Inventory.cs
+++ Bahar.DemoApp.InventoryService.Model/Inventory.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Bahar.DemoApp.InventoryService.Model
 {
-    public class Inventory:EntityBase<int>
+    public class Inventory : EntityBase<int>
     {
         public Inventory()
         {
@@ -12,10 +13,12 @@ namespace Bahar.DemoApp.InventoryService.Model
 
         public Inventory(string inventoryName, string currentAddress, string phoneNumber)
         {
+            ValidateInventoryName_Address(inventoryName, currentAddress);
             InventoryName = inventoryName;
             CurrentAddress = currentAddress;
-            PhoneNumber = phoneNumber;
 
+            ValidatePhoneNumber(phoneNumber);
+            PhoneNumber = string.Format("({0}) {1}-{2}", phoneNumber.Substring(0, 3), phoneNumber.Substring(3, 3), phoneNumber.Substring(6, 4));
 
 
         }
@@ -25,5 +28,39 @@ namespace Bahar.DemoApp.InventoryService.Model
         public string CurrentAddress { get; set; }
 
         public string PhoneNumber { get; set; }
+
+        public void ValidateInventoryName_Address(string Inventoryname, string currentAddress)
+        {
+            string errormessage = string.Empty;
+
+            if (string.IsNullOrEmpty(Inventoryname.Trim()))
+                errormessage = "Inventory Name can not be null or empty string";
+
+
+            if (Inventoryname.Count() > 64)
+                errormessage = "Inventory Name can not be more than 64 charachter";
+
+            if (string.IsNullOrEmpty(currentAddress.Trim()))
+                errormessage = "Address can not be empty or null string";
+
+            if (!string.IsNullOrEmpty(errormessage))
+                throw new ArgumentException(errormessage.Trim());
+        }
+
+        public void ValidatePhoneNumber(string phonenumber)
+        {
+            string errormessage = string.Empty;
+
+            if (string.IsNullOrEmpty(phonenumber.Trim()))
+                errormessage = "Phone Number can not be null or empty string";
+
+            else if (phonenumber.Count() < 10 || phonenumber.Count() > 10)
+                errormessage = "Phone Number should be just 10 character (No more or less)";
+
+
+            if (!string.IsNullOrEmpty(errormessage))
+                throw new ArgumentException(errormessage.Trim());
+
+        }
     }
 }
--- Model.Test/InventoryTest.cs
+++ Model.Test/InventoryTest.cs
@@ -9,22 +9,79 @@ namespace Model.Test
     {
         [Theory]
         [InlineData("Inventory Toronto")]
-        [InlineData("")]
-        [InlineData("                                                                                                                         ")]
         [InlineData("Inventory")]
         public void Ctor_Should_Assign_Name_Value_Properly(string name)
         {
             Inventory inventory = new Inventory(name, "address", "6478596321");
+            Assert.Equal(inventory.InventoryName, name);
+
         }
 
-        public void Ctor_Should_Assign_Phone_Number_Properly()
+        [Fact]
+        public void Empty_Name_Throws_Exception()
         {
-         
+            ArgumentException exception = Assert.
+                Throws<ArgumentException>(() => new Inventory("", "address1", "1234567893"));
+            Assert.Equal("Inventory Name can not be null or empty string", exception.Message);
         }
 
-        public void Ctor_Should_Assign_CurrentAddress_Properly()
+
+        [Theory]
+        [InlineData("6987458257412587458djxjxjjxjjxjxjjxjjxjjxjxjjxjxjxjjxjxjjxjxjxjjxjxjxjjxjxjkskskkskssskskskkskskskskkskskskskkskksksksk")]
+        public void InventoryName_More_Than64_Throws_Exception(string name)
         {
+            ArgumentException exception = Assert.
+               Throws<ArgumentException>(() => new Inventory(name, "address1", "1234567893"));
+            Assert.Equal("Inventory Name can not be more than 64 charachter", exception.Message);
+        }
+
+
+
+        [Theory]
+        [InlineData("Address One")]
+        [InlineData("Address")]
+        public void Ctor_Should_Assign_Address_Value_Properly(string currentAddress)
+        {
+            Inventory inventory = new Inventory("inventory Nmae", currentAddress, "6478596321");
+            Assert.Equal(inventory.CurrentAddress, currentAddress);
 
         }
+        [Fact]
+        public void Empty_Address_shoud_Throw_Exception()
+        {
+            ArgumentException exception = Assert.
+                Throws<ArgumentException>(() => new Inventory("Inventory Name", "", "1478523696"));
+            Assert.Equal("Address can not be empty or null string", exception.Message);
+        }
+
+
+        [Theory]
+        [InlineData("1234567893")]
+
+        public void Ctor_Should_Assign_Phone_Number_Properly(string phoneNumber)
+        {
+            Inventory inventory = new Inventory("inventory Nmae", "Address 1", phoneNumber);
+            var FormattedPhoneNumber = string.Format("({0}) {1}-{2}", phoneNumber.Substring(0, 3), phoneNumber.Substring(3, 3), phoneNumber.Substring(6, 4));
+            Assert.Equal(inventory.PhoneNumber, FormattedPhoneNumber);
+
+        }
+        [Fact]
+        public void Empty_PhoneNumber_shoud_Throw_Exception()
+        {
+            ArgumentException exception = Assert.
+                Throws<ArgumentException>(() => new Inventory("Inventory Name", "address ", ""));
+            Assert.Equal("Phone Number can not be null or empty string", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("12345678988883")]
+        [InlineData("123883")]
+        public void PhoneNumber_Less_Than_10_shoud_Throw_Exception(string phoneNumber)
+        {
+            ArgumentException exception = Assert.
+                Throws<ArgumentException>(() => new Inventory("Inventory Name", "address ", phoneNumber));
+            Assert.Equal("Phone Number should be just 10 character (No more or less)", exception.Message);
+        }
+
     }
 }
